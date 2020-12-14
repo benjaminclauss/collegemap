d8d9125007963021ba2e6f58a3026b19bc5af355
Benjamin Clauss <benjaminclauss@Benjamins-MacBook-Pro.local>
2020-12-13 22:10:00 -0500

    Prepend client environment variable with REACT_APP_

--- client/src/Colleges.jsx
+++ client/src/Colleges.jsx
@@ -14,6 +14,7 @@ class Colleges extends React.Component {
       isDialogOpen: false,
     };
     this.handleClickButton = this.handleClickButton.bind(this);
+    console.log(API);
   }
 
   componentDidMount() {
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export,no-multi-assign
-export const API = process.env.API_URL || 'http://localhost:4000/api';
+export const API = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
