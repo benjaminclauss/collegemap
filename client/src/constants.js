b94beb74f404565ee4447748f3f2a72d69eba8bf
Benjamin Clauss <benjaminclauss@protonmail.com>
2020-12-20 18:14:39 -0500

    Add back eslint

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export,no-multi-assign
-export const API = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
+// eslint-disable-next-line no-undef
+export const API = process.env.REACT_APP_API_URL || "http://localhost:4000/api";
